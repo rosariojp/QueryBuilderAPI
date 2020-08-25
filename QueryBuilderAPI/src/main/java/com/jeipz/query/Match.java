fc8d3557d94eb0712f916ee75b6f865f3dac9340
rosariojp <rosario.jeipz@gmail.com>
2020-08-25 18:08:14 +0800

    Added implementation to Match and Must class

--- QueryBuilderAPI/src/main/java/com/jeipz/query/Match.java
+++ QueryBuilderAPI/src/main/java/com/jeipz/query/Match.java
@@ -1,5 +1,22 @@
 package com.jeipz.query;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Match {
+
+	Map<String, String> match = new HashMap<String, String>();
+	
+	public Match(String key, String value) {
+		match.put(key, value);
+	}
+
+	public Map<String, String> getMatch() {
+		return match;
+	}
+
+	public void setMatch(Map<String, String> match) {
+		this.match = match;
+	}
 
 }
--- QueryBuilderAPI/src/main/java/com/jeipz/query/Must.java
+++ QueryBuilderAPI/src/main/java/com/jeipz/query/Must.java
@@ -1,5 +1,23 @@
 package com.jeipz.query;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class Must {
+	
+	List<Map<String, String>> must = new ArrayList<Map<String, String>>();
+	
+	public Must(Match match) {
+		must.add(match.getMatch());
+	}
+
+	public List<Map<String, String>> getMust() {
+		return must;
+	}
+
+	public void setMust(List<Map<String, String>> must) {
+		this.must = must;
+	}
 
 }
